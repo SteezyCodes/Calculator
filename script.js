2f10e3d8a658ee3ce88228be27f2b6d16eb24f61
SteezyCodes <bartkom2002@gmail.com>
2022-09-05 22:45:49 +0200

    eval problems on handleEquals, missing second number

---
+++ script.js
@@ -0,0 +1,72 @@
+let buttons = document.querySelectorAll(".number");
+let screenOutput = document.querySelector(".output");
+let clearButton = document.querySelector(".ac");
+let deleteButton = document.querySelector(".del");
+let addButton = document.querySelector(".plus");
+let solveButton = document.querySelector(".equals");
+let subtractButton = document.querySelector(".minus");
+
+let blockMultipleMathSigns = false;
+
+//displayed array of numbers with basic value
+let output = [0];
+
+buttons.forEach((button) => {
+  const handleButton = (btn) => {
+    output.push(Number(button.textContent));
+    if (output.length > 1 && output[0] === 0) {
+      output.shift();
+    }
+  };
+  button.addEventListener("click", handleButton());
+});
+
+const handleDelete = () => {
+  if (output.length !== 1) {
+    output.pop();
+  } else {
+    output = [0];
+  }
+};
+
+const handleClear = () => {
+  output = [0];
+  answer = undefined;
+  blockMultipleMathSigns = false;
+};
+
+const handleAdd = () => {
+  if (blockMultipleMathSigns === false) {
+    output.push(" + ");
+  }
+  blockMultipleMathSigns = true;
+};
+
+const handleSubtract = () => {
+  if (blockMultipleMathSigns === false) {
+    output.push(" - ");
+  }
+  blockMultipleMathSigns = true;
+};
+
+const handleEquals = () => {
+  output = output.join("");
+  output = output.toString("");
+  console.log(typeof output);
+  console.log(output);
+  answer = eval(output);
+  console.log(answer);
+  answer = answer.toString().split("");
+  output = answer.map(Number);
+  blockMultipleMathSigns = false;
+};
+
+deleteButton.addEventListener("click", handleDelete());
+clearButton.addEventListener("click", handleClear());
+addButton.addEventListener("click", handleAdd());
+subtractButton.addEventListener("click", handleSubtract());
+solveButton.addEventListener("click", handleEquals());
+
+setInterval(() => {
+  screenOutput.textContent = output.join("");
+}, 200);
